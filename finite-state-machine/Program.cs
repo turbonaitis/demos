b87ce83ae56d87138a40f93c906b2d9f27527509
Tomas <turbon@gmail.com>
2020-07-04 21:59:40 +0300

    Generated new console project

---
+++ finite-state-machine/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace finite_state_machine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
